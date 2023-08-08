62f29c7ea65fb7ee26e1fd4b183512c32f72561c
arismoratalla <arismoratalla@gmail.com>
2023-08-08 17:45:12 +0800

    removed if-block for html header

--- src/routes.js
+++ src/routes.js
@@ -9,17 +9,17 @@ export default function buildRoutes (route) {
 
   // Error 404 Handler
   route.use((req, res) => {
-    if (req.headers.accept.includes('html')) {
-      return res.status(404).send(`
-        <div>
-          <span>Page not found.</span>
-        </div>
-      `)
-    } else {
+    // if (req.headers.accept.includes('html')) {
+    //   return res.status(404).send(`
+    //     <div>
+    //       <span>Page not found.</span>
+    //     </div>
+    //   `)
+    // } else {
       return res.status(404).json({
         success: false,
         message: 'API not found.'
       })
-    }
+    // }
   })
 }
