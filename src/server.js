16bcb83dc527d152c150dc0317972824c88b87fd
arismoratalla <arismoratalla@gmail.com>
2023-09-01 23:37:33 +0800

    trying to fix dotenv error

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
-import 'dotenv/config'
-
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
+import 'dotenv/config'
 
 import buildRoutes from './routes'
 import { databaseConnection } from './utilities/mysqldb'
