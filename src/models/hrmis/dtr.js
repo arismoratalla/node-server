101db2ecbe59c7b25bf011ce63da562d549d6126
arismoratalla <arismoratalla@gmail.com>
2023-09-01 14:53:14 +0800

    removed semicolons

--- src/models/hrmis/dtr.js
+++ src/models/hrmis/dtr.js
@@ -2,12 +2,12 @@ import { Sequelize, Model, Op } from 'sequelize'
 import sequelize from '../../utilities/hrmisdb'
 import Employee from './employee'
 
-const date = new Date();
-const year = date.getFullYear();
-const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
-const day = String(date.getDate()).padStart(2, '0');
+const date = new Date()
+const year = date.getFullYear()
+const month = String(date.getMonth() + 1).padStart(2, '0') // Months are 0-indexed
+const day = String(date.getDate()).padStart(2, '0')
 
-const thisDay = `${year}-${month}-${day}`;
+const thisDay = `${year}-${month}-${day}`
 
 class Dtr extends Model {
   static async earlybirds () {
